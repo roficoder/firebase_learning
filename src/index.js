a845fb5b83479c58eff9d4085daba3f73bdf3b12
AbdulRahman937 <againinus786@gmail.com>
2023-07-30 19:18:29 +0500

    up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDZFLK53alqSt6_vck9MI55HCd1dFRp7L0",
@@ -19,13 +26,85 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, "tooltips");
 
+getData();
 // get collection data
-getDocs(colRef).then((snapshot) => {
-  let tooltips = [];
-  snapshot.docs.forEach((tip)=>{
-    tooltips.push({...tip.data(), id: tip.id})
+function getData() {
+  getDocs(colRef)
+    .then((snapshot) => {
+      let tooltips = [];
+      snapshot.docs.forEach((tip) => {
+        tooltips.push({ ...tip.data(), id: tip.id });
+      });
+      console.log(tooltips);
+      tableContainer.appendChild(createTable(tooltips));
+    })
+    .catch(() => {
+      console.log("Error");
+    });
+}
+
+function submitData() {
+  const name = document.getElementById("name");
+  const description = document.getElementById("description");
+  const link = document.getElementById("link");
+
+  console.log(name, description, link);
+}
+
+const tableContainer = document.getElementById("tableContainer");
+
+function createTable(data) {
+  const table = document.createElement("table");
+  table.classList.add('table', 'table-striped');
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
+  const headerRow = document.createElement("tr");
+
+  const th = `
+  <th>Title</th>
+  <th>Description</th>
+  <th>Link</th>
+  <th>Action</th>
+  `;
+  headerRow.innerHTML = th;
+
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+
+  // Create table rows
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    let html = `
+    <td>${item.title}</td>
+    <td>${item.description}</td>
+    <td class="link-cell"><a href="${item.link}">Link</a></td>
+    <td><button type="button" class="btn btn-danger"><span class="material-symbols-outlined">delete</span></button><button type="button" class="btn btn-primary"><span class="material-symbols-outlined">edit</span></button></td>
+    `;
+
+    row.innerHTML = html
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+  return table;
+}
+
+const addForm = document.getElementById("addForm");
+addForm
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    addDoc(colRef, {
+      title: addForm.name.value,
+      description: addForm.description.value,
+      link: addForm.link.value,
+    });
   })
-  console.log(tooltips);
-}).catch(()=>{
-  console.log('Error');
-})
+  .then(() => {
+    addForm.reset();
+    window.location.replace("http://127.0.0.1:5500/dist/index.html");
+    getData();
+  })
+  .catch(() => {
+    console.log("Error");
+  });
